ae28d1b7a7967b6bc8036d32786fb9ee281f218b
Chase090 <carloyam21@icloud.com>
2021-07-13 19:35:33 -0700

    commit tree bf4781d33b808423c36bda8a3eaa2d2806e9c6f6

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Add your code here
+// fetch(destinationURL, configurationObject);
+
+const { restore } = require("chai-spies");
+
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+// };
+
+// const configurationObject = { 
+    // method: "POST",
+    // headers: {
+        // "Content-Type": "application/json",
+        // * what type of data we are sending
+        // "Accept": "application/json"
+        // * what type of data we want to recieve
+//     },
+//     body: JSON.stringify(formData)
+// };
+
+// fetch('http://localhost:3000/dogs', configurationObject)
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(object) {
+//         console.log(object);
+//     })
+//     .catch(function(error) {
+//         alert("Bad things! Ragnarők!");
+//         console.log(error.message);
+//       }); 
+
+// write a function that user and email
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+    .then(resp => resp.json())
+    .then(data => document.body.innerHTML = data.id)
+    .catch(error => document.body.innerHTML = error.message)
+
+}
